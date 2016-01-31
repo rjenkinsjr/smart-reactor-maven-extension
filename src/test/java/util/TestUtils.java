1f0edab0d1022c1a0ab43aa73c112ad49e1c3b28
rjenkinsjr <me@ronjenkins.info>
2016-01-31 16:00:07 -0500

    Remove unused testing utility methods.

--- src/test/java/util/TestUtils.java
+++ src/test/java/util/TestUtils.java
@@ -9,16 +9,9 @@ import java.util.List;
 import java.util.Map;
 
 import mockit.Deencapsulation;
-import mockit.Mock;
-import mockit.MockUp;
 
 import org.apache.commons.lang.ObjectUtils;
 import org.apache.commons.lang.Validate;
-import org.apache.maven.artifact.Artifact;
-import org.apache.maven.artifact.DefaultArtifact;
-import org.apache.maven.artifact.handler.ArtifactHandler;
-import org.apache.maven.model.Model;
-import org.apache.maven.project.MavenProject;
 import org.apache.maven.shared.release.config.ReleaseDescriptor;
 import org.apache.maven.shared.release.env.ReleaseEnvironment;
 import org.apache.maven.shared.release.phase.ReleasePhase;
@@ -90,114 +83,6 @@ public final class TestUtils {
 	Deencapsulation
 		.setField(step, "releaseEnvironment", releaseEnvironment);
 	return logger;
-    }
-
-    /**
-     * Returns a new MavenProject object whose GAV matches the given parameters.
-     * The scope is "compile", the type/packaging is "jar" and the classifier is
-     * blank.
-     * 
-     * <p>
-     * The underlying {@link Model} and {@link ArtifactHandler} are mocked, but
-     * the underlying {@link Artifact} is not.
-     * 
-     * @param groupId
-     *            not null.
-     * @param artifactId
-     *            not null.
-     * @param version
-     *            not null.
-     * @return never null.
-     */
-    public static MavenProject mockMavenProject(final String groupId,
-	    final String artifactId, final String version) {
-	return mockMavenProject(groupId, artifactId, version, "compile", "jar",
-		"");
-    }
-
-    /**
-     * Returns a new MavenProject object whose scope and coordinate matches the
-     * given parameters.
-     * 
-     * <p>
-     * The underlying {@link Model} and {@link ArtifactHandler} are mocked, but
-     * the underlying {@link Artifact} is not.
-     * 
-     * @param groupId
-     *            not null.
-     * @param artifactId
-     *            not null.
-     * @param version
-     *            not null.
-     * @param scope
-     *            not null.
-     * @param type
-     *            not null.
-     * @param classifier
-     *            not null.
-     * @return never null.
-     */
-    public static MavenProject mockMavenProject(final String groupId,
-	    final String artifactId, final String version, final String scope,
-	    final String type, final String classifier) {
-	Validate.notNull(groupId, "groupId is null");
-	Validate.notNull(artifactId, "artifactId is null");
-	Validate.notNull(version, "version is null");
-	Validate.notNull(scope, "scope is null");
-	Validate.notNull(type, "type is null");
-	Validate.notNull(classifier, "classifier is null");
-	final Model model = new MockUp<Model>() {
-	    @Mock
-	    String getGroupId() {
-		return groupId;
-	    }
-
-	    @Mock
-	    String getArtifactId() {
-		return artifactId;
-	    }
-
-	    @Mock
-	    String getVersion() {
-		return version;
-	    }
-
-	    @Mock
-	    String getPackaging() {
-		return type;
-	    }
-
-	    @Mock
-	    public Model clone() {
-		return this.getMockInstance();
-	    }
-	}.getMockInstance();
-	final ArtifactHandler ah = new MockUp<ArtifactHandler>() {
-	    @Mock
-	    String getExtension() {
-		return type;
-	    }
-
-	    @Mock
-	    String getDirectory() {
-		return "";
-	    }
-
-	    @Mock
-	    String getClassifier() {
-		return classifier;
-	    }
-
-	    @Mock
-	    String getPackaging() {
-		return type;
-	    }
-	}.getMockInstance();
-	final Artifact artifact = new DefaultArtifact(groupId, artifactId,
-		version, scope, type, classifier, ah);
-	final MavenProject project = new MavenProject(model);
-	project.setArtifact(artifact);
-	return project;
     }
 
     /** Uninstantiable. */
