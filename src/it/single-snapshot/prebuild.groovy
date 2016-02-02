922fc93989aaf75f979bcbe0822484eb28528eea
rjenkinsjr <me@ronjenkins.info>
2016-02-01 23:48:31 -0500

    Further consolidate/improve prebuild logic.

--- src/it/single-snapshot/prebuild.groovy
+++ src/it/single-snapshot/prebuild.groovy
@@ -11,4 +11,4 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-evaluate(new java.io.File(itResourcesDir, 'prebuild.groovy'))
+evaluate(new java.io.File(itCommonPrebuild))
